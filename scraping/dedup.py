832b0cea1cabfd4b53dba047a2e2f7e8c0dc68b8
VakeDomen <vake.domen94@gmail.com>
2024-06-21 13:51:43 +0200

    moved the pipeline

--- scraping/dedup.py
+++ scraping/dedup.py
@@ -1,32 +1,39 @@
 import pandas as pd
 from tqdm import tqdm
+import sys
+
+input_path = sys.argv[1]
+output_path = sys.argv[2]
 
 
-def process_csv(input_file):
+def process_csv(input_file, output_file):
     # Read the CSV file
-    df = pd.read_csv(input_file)
-    
+    df = pd.read_csv(input_file, sep=";")   
+    print(df)
+    df = df.sort_values(by=['ModelHub', "Average ⬆️"], ascending=False)
+    new_df = pd.DataFrame(columns=df.columns)
     # Initialize last valid row from the first row
-    last_valid_row = df.iloc[0, 2:].copy()
+    last_valid_row = df.iloc[0, :].copy()
     
+    
+
     # Iterate over the DataFrame, starting from the second row
     for i in tqdm(range(1, len(df))):
         # Get the current row starting from the third column
-        current_row = df.iloc[i, 2:].copy()
+        current_row = df.iloc[i, :].copy()
+        # print(f"{current_row['ModelHub']} --  {last_valid_row['ModelHub']}")
         # Compare the current row with the last valid row
-        if current_row.equals(last_valid_row):
-            # If they are the same, clear the current row values from the third column onwards
-            df.iloc[i, 2:] = ""
-        else:
+        if not current_row['ModelHub'] == last_valid_row['ModelHub']:
             # Update the last valid row to the current row
+            new_df = new_df.append(last_valid_row, ignore_index=True)
             last_valid_row = current_row
 
+
     # Define the output file name
-    output_file = input_file.replace(".csv", "_modified.csv")
     
     # Write the modified DataFrame to a new CSV file, preserving the index if it's part of the data
-    df.to_csv(output_file, index=False)
+    new_df.to_csv(output_file, index=False, sep=";")
     print(f"Modified file saved as {output_file}")
 
 
-process_csv("output.csv")
+process_csv(input_path, output_path)
